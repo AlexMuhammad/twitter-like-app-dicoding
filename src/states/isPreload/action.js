f51b8c9f7acaac7c3fd7e90f1f7b785a3fe04f70
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2023-11-28 23:18:02 +0700

    feat: creating action and reducer for isPreload

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,37 @@
+import api from "../../utils/api"
+import { setAuthUserActionCreator } from "../authUser/action"
+
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+const ActionType = {
+    SET_IS_PRELOAD: 'SET_IS_PRELOAD'
+}
+
+const setIsPreloadActionCreator = (isPreload) => {
+    return{
+        type: ActionType.SET_IS_PRELOAD,
+        payload: {
+            isPreload
+        }
+    }
+}
+
+const asyncPreloadProcess = () => {
+    return async (dispatch) => {
+        try {
+            const authUser = await api.getOwnProfile();
+            dispatch(setAuthUserActionCreator(authUser))
+        } catch (error) {
+            dispatch(setAuthUserActionCreator(null))
+        }finally{
+            dispatch(setIsPreloadActionCreator(false))
+        }
+    }
+}
+
+export {
+    ActionType,
+    asyncPreloadProcess,
+    asyncPreloadProcess
+}
