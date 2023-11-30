6541586e78e8fcedc62ae9bf278d9924c0e4377a
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2023-11-30 09:15:26 +0700

    creating shared action fro users and talks

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,24 @@
+import api from "../../utils/api"
+import { receiveTalksActionCreator } from "../talks/action";
+import { receiveUsersActionCreator } from "../users/action";
+
 /**
  * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
  */
+function asyncPopulateUsersAndTalks(){
+    return async (dispatch) => {
+        try {
+            const users = await api.getAllUsers();
+            const talks = await api.getAllTalks();
+
+            dispatch(receiveTalksActionCreator(talks));
+            dispatch(receiveUsersActionCreator(users));
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+}
+
+export {
+    asyncPopulateUsersAndTalks
+}
